77677ceb1c890196aa6b3d84da3792fae41a7fa3
Strallia Chao <134454347+leeyuh8@users.noreply.github.com>
2023-12-28 13:35:48 -0800

    Run custom function with form input values when click Add Book button

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
+// DIALOG BUTTONS
 const dialog = document.querySelector("dialog");
 const openDialogBtn = document.querySelector(".open-dialog");
 const closeDialogBtn = document.querySelector(".close-dialog");
+const addBookBtn = document.querySelector(".add-book");
 
+
+// FORM INPUTS
+const titleInput = document.querySelector("#title");
+const authorInput = document.querySelector("#author");
+const pagesInput = document.querySelector("#pages");
+const readStatusInput = document.querySelector("#read-status");
+
+
+// EVENT LISTENERS
 openDialogBtn.addEventListener('click', () => dialog.showModal());
 closeDialogBtn.addEventListener('click', () => dialog.close());
+addBookBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+  let obj = {};
+    obj.title = titleInput.value;
+    obj.author = authorInput.value;
+    obj.pages = pagesInput.value;
+    if (readStatusInput.checked) { 
+      obj.readStatus = readStatusInput.value; 
+    } else {
+      obj.readStatus = "";
+    }
+  btnTestFunction(obj);
+  dialog.close();
+})
+
+function btnTestFunction(obj) {
+  console.log("Run btnTestFunction with form inputs:", obj);
+}
+
