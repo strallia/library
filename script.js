6d9ff41599eee30ec40ace03ebd73b7c5aa5cd27
Strallia Chao <134454347+leeyuh8@users.noreply.github.com>
2023-12-29 10:27:03 -0800

    Render each book from myLibrary array

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
 const myLibrary = [];
 
-// DIALOG BUTTONS
+// DIALOG and FORM
 const dialog = document.querySelector("dialog");
 const openDialogBtn = document.querySelector(".open-dialog");
 const closeDialogBtn = document.querySelector(".close-dialog");
 const addBookBtn = document.querySelector(".add-book");
 
-
-// FORM INPUTS
 const form = document.querySelector("form");
 const titleInput = document.querySelector("#title");
 const authorInput = document.querySelector("#author");
 const pagesInput = document.querySelector("#pages");
 const readStatusInput = document.querySelector("#read-status");
 
-
-// EVENT LISTENERS
 openDialogBtn.addEventListener('click', () => dialog.showModal());
 closeDialogBtn.addEventListener('click', () => {
   form.reset();
@@ -29,17 +25,19 @@ addBookBtn.addEventListener('click', (event) => {
   } else {
     readStatusValue = "";
   };
-  myLibrary.push( new Book(
+  myLibrary.unshift(new Book(
     titleInput.value,
     authorInput.value,
     pagesInput.value,
     readStatusValue
   ));
+  renderBooks(myLibrary);
   form.reset();
   dialog.close();
 });
 
 
+// BOOKS
 function Book(title, author, pages, readStatus) {
   this.title = title;
   this.author = author;
@@ -48,4 +46,28 @@ function Book(title, author, pages, readStatus) {
   this.info = function() {
     return `${title} by ${author}, ${pages} pages, ${readStatus}`;
   }
+}
+
+const shelfDiv = document.querySelector('.shelf');
+function renderBooks(myLibrary) {
+  // clear shelf
+  const currentBooks = document.querySelectorAll('.book');
+  if (currentBooks) {
+    currentBooks.forEach((book) => {
+      shelfDiv.removeChild(book);
+    });
+  };
+  
+  // render each book
+  myLibrary.forEach((book) => {
+    const bookDiv = document.createElement('div');
+    bookDiv.textContent = `
+      Title: ${book.title},
+      Author: ${book.author},
+      Pages: ${book.pages},
+      Read Status: ${book.readStatus}
+    `;
+    bookDiv.classList.add('book');
+    shelfDiv.appendChild(bookDiv);
+  });
 }
